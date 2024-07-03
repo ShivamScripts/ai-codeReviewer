df35949b0590cb94c569c18355b985bee1f8a421
ShivamScripts <shivamaggarwal767@gmail.com>
2024-07-03 17:50:39 +0530

    new commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,6 @@ export class AppService {
 
   get(): string {
     console.log('hello world, how is ig!')
-    return 'Hello Prima, how is the weer! are , what dong.';
+    return 'Hello Prima, how is the weer! are , whatong.';
   }
 }
