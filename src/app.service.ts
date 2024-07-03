05c0e16a788cbe9b400166238559a943a3430cb9
ShivamScripts <shivamaggarwal767@gmail.com>
2024-07-03 15:46:43 +0530

    test commit 12

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,6 @@ export class AppService {
 
   get(): string {
     console.log('hello world, how is it going!')
-    return 'Hello Prima, how is the weather! how are you, what are you doing. what can i do for you today are you recieving we hook event';
+    return 'Hello Prima, how is the weather! how are you, what are you doing.';
   }
 }
