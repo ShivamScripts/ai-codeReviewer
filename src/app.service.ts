5c2ed0624e489abc1949b89ef84414017b619f5e
ShivamScripts <shivamaggarwal767@gmail.com>
2024-07-09 14:51:05 +0530

    test commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -9,6 +9,6 @@ export class AppService {
   get(): string {
     console.log('hello world, how is ig!')
     const maths = 1 +2+5;
-    return `${maths} Hello Prima,w is th da y ple tl me eerbibi, whong.`;
+    return `${maths} Hello Prima,w is th da y ple tl me eeibi, whong.`;
   }
 }
